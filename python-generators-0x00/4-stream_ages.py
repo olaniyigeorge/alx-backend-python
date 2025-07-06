a1ab9940e1ddf6540dbe1c647152b686efb50641
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-06 11:54:54 +0100

    Initial commit

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,13 @@
+# Objective: to use a generator to compute a memory-efficient aggregate function i.e average age for a large dataset
+
+# Instruction:
+
+# Implement a generator stream_user_ages() that yields user ages one by one.
+
+# Use the generator in a different function to calculate the average age without loading the entire dataset into memory
+
+# Your script should print Average age of users: average age
+
+# You must use no more than two loops in your script
+
+# You are not allowed to use the SQL AVERAGE
