c9b1e70d6cf8e5c261e54fe283efe71ee5be745e
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-09 18:48:17 +0100

    class based ctx manager

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,25 +1,33 @@
 import sqlite3
-from contextlib import contextmanager
 
-@contextmanager
-def execute_query(db_name, query, params=None):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    try:
-        cursor.execute(query, params or [])
-        results = cursor.fetchall()
-        yield results
-        conn.commit()
-    except Exception as e:
-        conn.rollback()
-        print(f"Query failed: {e}")
-        raise
-    finally:
-        conn.close()
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or []
+        self.conn = None
+        self.cursor = None
+        self.result = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        self.result = self.cursor.fetchall()
+        return self.result
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            if exc_type:
+                self.conn.rollback()
+                print(f"Error: {exc_value}")
+            else:
+                self.conn.commit()
+            self.conn.close()
 
 
-query = "SELECT * FROM User WHERE first_name=?"
-params = ("Og",)
+query = "SELECT * FROM users WHERE age > ?"
+params = (25,)
 
-with execute_query("users.db", query, params) as result:
+with ExecuteQuery("users.db", query, params) as result:
     print(result)
