4cfe6dade24198e30d910fd33e0cce72b4dd095c
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-26 17:07:22 +0100

    swagger docs and serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+def index(request): 
+    """
+    Render the index page for the chat application.
+    """   
+    return HttpResponse("<h1>Welcome to the Chat Application!</h1>")
