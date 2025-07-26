400407cd777d18572fd72cbc3a5846d2d4fbcb9c
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-26 10:39:55 +0100

    envs client

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -169,8 +169,7 @@ REST_FRAMEWORK = {
 
 # CORS Headers
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    env('CLIENT_DOMAIN')
 ]
 CORS_ALLOW_CREDENTIALS = True
 
