1d6c75db2a77fcab2c26944fecefd7727f521f95
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-31 14:00:41 +0100

    caching

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from requests import request
@@ -91,3 +92,34 @@ def inbox_unread(request):
 
     data = [{"content": msg.content, "timestamp": msg.timestamp} for msg in unread_msgs]
     return JsonResponse({"unread_messages": data})
+
+
+
+
+
+@cache_page(60)  
+@login_required
+def conversation_messages(request):
+    """
+    View that returns all messages between the logged-in user and another user.
+    Cached for 60 seconds to improve performance.
+    """
+    other_user_id = request.GET.get("user_id")
+    if not other_user_id:
+        return JsonResponse({"error": "Missing user_id"}, status=400)
+
+    messages = Message.objects.filter(
+        sender_id__in=[request.user.id, other_user_id],
+        reciever_id__in=[request.user.id, other_user_id]
+    ).order_by("timestamp").only("content", "timestamp", "sender_id")
+
+    data = [
+        {
+            "sender_id": msg.sender_id,
+            "content": msg.content,
+            "timestamp": msg.timestamp
+        }
+        for msg in messages
+    ]
+
+    return JsonResponse({"messages": data})
