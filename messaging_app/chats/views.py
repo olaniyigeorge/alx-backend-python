b186d5ac513605f1bac7e05331ae09fe28684685
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-08-09 13:56:39 +0100

    app_permisions2

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -137,23 +137,23 @@ class MessageViewSet(viewsets.ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user not in instance.conversation.participants.all():
-            return Response({"detail": "You are not a participant in this conversation."}, status=HTTP_403_FORBIDDEN)
+            return Response({"detail": "You are not a participant in this conversation."}, status=status.HTTP_403_FORBIDDEN)
         return super().retrieve(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         conversation = serializer.validated_data.get("conversation")
         if self.request.user not in conversation.participants.all():
-            return Response({"detail": "You cannot send messages in this conversation."}, status=HTTP_403_FORBIDDEN)
+            return Response({"detail": "You cannot send messages in this conversation."}, status=status.HTTP_403_FORBIDDEN)
         serializer.save(sender=self.request.user)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user not in instance.conversation.participants.all():
-            return Response({"detail": "You cannot update messages in this conversation."}, status=HTTP_403_FORBIDDEN)
+            return Response({"detail": "You cannot update messages in this conversation."}, status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user not in instance.conversation.participants.all():
-            return Response({"detail": "You cannot delete messages in this conversation."}, status=HTTP_403_FORBIDDEN)
+            return Response({"detail": "You cannot delete messages in this conversation."}, status=status.HTTP_403_FORBIDDEN)
         return super().destroy(request, *args, **kwargs)
